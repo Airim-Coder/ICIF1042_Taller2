8969431ec3a8861dc775e94cdd6bc60e262275b4
airi0x02 <airim.coder@gmail.com>
2024-06-03 02:55:01 -0400

    corregir errores de generacion de ventas y agregar sexo a la lista de nombres

--- datos.py
+++ datos.py
@@ -9,12 +9,33 @@ Contenido:
     - La columna "Producto" puede contener hasta un 10% de valores nulos. 
     - En el archivo "clientes.csv", las columnas "Nombre" y "Edad" pueden contener hasta un 5% de valores nulos cada una.
 '''
-import faker as fk 
+from faker import Faker
 import pandas as pd
 import numpy as np
-import os, random, csv
+import os, random
 
-def DataGen_Cliente(num_clientes):
+def Simulador_Ventas():
+    print("SIMULADOR DE VENTAS")
+    num_clientes = int(input("Para comenzar, Cuántos Clientes desea?\nConsidere a lo menos 50: ")) # Numero de clientes
+    num_ventas = int(input("Cuántas ventas desea evaluar?\nConsidere a lo menos 100': "))
+    
+    while(True):  #mientras las filas sean menor a 50
+        if num_clientes < 50 :
+            print("Para que el analisis sea mas eficiente considere a lo menos 50 clientes")
+            num_clientes = int(input("Cuántos clientes desea?:")) #volver a ingresar numero de filas
+        else:
+            clientes = DataGen_Clientes(num_clientes)
+            FileWrite('clientes.csv', clientes)
+                 
+            if num_ventas < 100 :
+                print("Para que el analisis sea mas eficiente considere a lo menos 100 ventas")
+                num_ventas = int(input("Cuántos ventas desea evaluar?:")) #volver a ingresar numero de filas
+            else:
+                ventas = DataGen_Ventas(num_ventas, clientes)
+                FileWrite('ventas.csv',ventas)
+                break
+
+def DataGen_Clientes(num_clientes):
     #region Generacion de datos de clientes
     nombres = NameListGen(num_clientes) #Generación de nombre para 125 clientes
     edad = range(18,80) #Generacion de Edad entre los 18 y 80 años
@@ -26,33 +47,67 @@ def DataGen_Cliente(num_clientes):
             "ID_Cliente": np.arange(1, num_clientes +1),
             "Nombre": [random.choices(nombres) if random.random() > 0.05 else "NULL" for i in range(num_clientes)],
             "Edad": [random.choices(edad) if random.random() > 0.05 else "NULL" for i in range(num_clientes)],
-            "Genero": [random.choice(genero)],
-            "Ubicacion": [random.choice(ubicacion)]
+            "Genero": [random.choice(genero) for i in range(num_clientes)],
+            "Ubicacion": [random.choice(ubicacion)for i in range(num_clientes)]
         })
         return clientes
     except:
         print("Ha surgido un error en la generacion de clientes")
 
-def DataGen_Ventas(num_ventas):
+def DataGen_Ventas(num_ventas, clientes):
     #region Generacion de datos de ventas
-    #ID_Venta,ID_Cliente,Producto,Categoría,Precio,Cantidad
+    categoria_aleatoria = random.choice(list(ProductList().keys()))#Seleccionar una categoría aleatoria
+    productos_categoria = ProductList()[categoria_aleatoria]["Productos"]#Obtener la lista de productos de la categoría aleatoria
+    producto_aleatorio = random.choice(list(productos_categoria.keys()))#Seleccionar un producto aleatorio de la categoría
+    precio_producto = productos_categoria[producto_aleatorio]["Precio"]#Obtener el precio del producto aleatorio
+    #endregion
     try:
-         ventas = pd.DataFrame({})
+         ventas = pd.DataFrame({
+             "ID_Venta": np.arange(1, num_ventas +1),
+             "ID_Cliente": list(clientes['ID_Cliente']),
+             "Producto": [producto_aleatorio if random.random() > 0.1 else "NULL" for i in range(num_ventas)],
+             "Categoría": productos_categoria,
+             "Precio": precio_producto,
+             "Cantidad": np.random.randint(1, 5, size=num_ventas)
+         })
          return ventas
     except:
         print("Ha surgido un error en la generacion de ventas")
-    #endregion
-        
+
+def FileWrite(nombre,datos): #Escribe los datos en un archivo CSV
+    try:
+        # Verifica si existe archivo CSV y lo elimina
+        if os.path.exists(nombre):# Elimina archivo CSV si existe en el directorio
+            os.remove(nombre)
+
+        # Genera un nuevo archivo CSV con datos
+        datos.to_csv(nombre, index=False)
+        print("Archivo CSV generado con exito")
+    except:
+        print("Ha surgido un error escribiendo el archivo")
 
 #region INTERNAL
 def NameListGen(clientes):
+    fake = Faker()
     nombres_comunes=[]
     for i in range(clientes):
-        name = fk.Faker().name()
+        name = fake.name()
         nombres_comunes.append(name)
     return nombres_comunes
-
-def ProductsListGen():
-    print()
+def ProductList():
+    categorias_productos = {
+        "Hogar": {
+            "Productos": {"Refrigerador":{"Precio": 199990},"Estufa Electrica":{"Precio": 129990}, "Lavadora":{"Precio": 164990}, "Horno Electrico":{"Precio": 189990}, "Lámpara":{"Precio": 4990}}},
+            "Electrónica": {
+            "Productos": {"Televisor":{"Precio": 159990},
+            "Laptop":{"Precio": 699990}, "Computadora":{"Precio": 360608}, "Tablet":{"Precio": 179990}, "Telefono":{"Precio": 296404}, "Licuadora":{"Precio": 19990}, "Batidora":{"Precio": 29990}, "Cafetera":{"Precio": 59990}, "Tostadora":{"Precio": 24990}, "Audífonos":{"Precio": 12990}, "Cámara":{"Precio": 60490}}  
+          }
+        }
+    return categorias_productos
 #endregion
 
+def main(): #Solo para depuracion
+    Simulador_Ventas()
+
+if __name__ == "__main__":
+    main()
