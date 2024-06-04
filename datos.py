1ce8826ecc9bb8c1fe6da3f6e07cefcbe97d38e4
airi0x02 <airim.coder@gmail.com>
2024-06-04 02:00:00 -0400

    hot fix clientes, nuevos errores a la vista

--- datos.py
+++ datos.py
@@ -37,22 +37,21 @@ def Simulador_Ventas():
 
 def DataGen_Clientes(num_clientes):
     #region Generacion de datos de clientes
-    nombres = NameListGen(num_clientes) #Generación de nombre para 125 clientes
-    edad = range(18,80) #Generacion de Edad entre los 18 y 80 años
-    genero = ['Femenino','Masculino']
-    ubicacion = ['Norte','Sur','Este','Oeste']
+    Clientes = NameListGen(num_clientes) #Generación de nombre para 125 clientes
     #endregion
+    
     try:
-        clientes = pd.DataFrame({
-            "ID_Cliente": np.arange(1, num_clientes +1),
-            "Nombre": [random.choices(nombres) if random.random() > 0.05 else "NULL" for i in range(num_clientes)],
-            "Edad": [random.choices(edad) if random.random() > 0.05 else "NULL" for i in range(num_clientes)],
-            "Genero": [random.choice(genero) for i in range(num_clientes)],
-            "Ubicacion": [random.choice(ubicacion)for i in range(num_clientes)]
-        })
+        for name, data in Clientes.items():
+            clientes = pd.DataFrame({
+                "ID_Cliente": np.arange(1, num_clientes +1),
+                "Nombre": name,
+                "Genero": data['Genero'],
+                "Edad": data['Edad'],
+                "Ubicacion": data['Ubicacion'],
+            })
         return clientes
-    except:
-        print("Ha surgido un error en la generacion de clientes")
+    except :
+        raise Exception("Ha surgido un error en la generacion de clientes")
 
 def DataGen_Ventas(num_ventas, clientes):
     #region Generacion de datos de ventas
@@ -64,15 +63,15 @@ def DataGen_Ventas(num_ventas, clientes):
     try:
          ventas = pd.DataFrame({
              "ID_Venta": np.arange(1, num_ventas +1),
-             "ID_Cliente": list(clientes['ID_Cliente']),
-             "Producto": [producto_aleatorio if random.random() > 0.1 else "NULL" for i in range(num_ventas)],
-             "Categoría": productos_categoria,
-             "Precio": precio_producto,
+             "ID_Cliente": list(clientes['ID_Cliente'] for i in range(num_ventas)),
+             "Producto": [random.choice(list(ProductList().keys())) if random.random() > 0.1 else "NULL" for i in range(num_ventas)],
+             "Categoría": [ProductList()[categoria_aleatoria]["Productos"] for i in range(num_ventas)],
+             "Precio": [precio_producto for i in range(num_ventas)],
              "Cantidad": np.random.randint(1, 5, size=num_ventas)
          })
          return ventas
     except:
-        print("Ha surgido un error en la generacion de ventas")
+        raise Exception("Ha surgido un error en la generacion de ventas")
 
 def FileWrite(nombre,datos): #Escribe los datos en un archivo CSV
     try:
@@ -89,10 +88,14 @@ def FileWrite(nombre,datos): #Escribe los datos en un archivo CSV
 #region INTERNAL
 def NameListGen(clientes):
     fake = Faker()
-    nombres_comunes=[]
-    for i in range(clientes):
-        name = fake.name()
-        nombres_comunes.append(name)
+    nombres_comunes={}
+    ubicacion = ['Norte','Sur','Este','Oeste']
+    for i in range(int(clientes/2)):
+        name = fake.name_female()
+        nombres_comunes= {'Nombre':name,'Genero': 'Femenino', 'Edad': random.choice(range(18,80)),'Ubicacion':random.choice(ubicacion)}
+    for i in range(int(clientes/2)):
+        name = fake.name_male()
+        nombres_comunes= {'Nombre':name,'Genero': 'Masculino', 'Edad': random.choice(range(18,80)),'Ubicacion':random.choice(ubicacion)}
     return nombres_comunes
 def ProductList():
     categorias_productos = {
