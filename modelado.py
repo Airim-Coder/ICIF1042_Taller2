897e32a8043398541290b74761527e721b9c830f
airi0x02 <airim.coder@gmail.com>
2024-06-09 19:18:02 -0400

    Añadidas bsas limpiar datos y modelado

---
+++ modelado.py
@@ -0,0 +1,19 @@
+'''
+Autor: Maria Paz Cisternas Pardo
+Version: 24.06.09
+Contenido:
+1. Cruzar los DataFrames de películas y ratings utilizando la columna "ID_Venta" como clave primaria.
+2. Calcular el rating promedio de cada película utilizando una Serie de Pandas.
+3. Generar una tabla que muestre la cantidad de ventas por Categoria y Precio utilizando las funciones de agregación de Pandas.
+4. Identificar los 10 Productos con mayor venta y los 10 productos con promedio más bajo utilizando indexación fancy y slicing en el DataFrame cruzado.
+'''
+
+
+def Estadistico_Ventas():
+    print()
+
+def main(): #Solo para depuracion
+    Estadistico_Ventas()
+
+if __name__ == "__main__":
+    main()
